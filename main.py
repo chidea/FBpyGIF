e06a2eecde33964a1bc63ac28246b2862bccaf3e
CH.Idea <sbw228@gmail.com>
2016-07-31 17:49:26 +0900

    argument parser detached from main

--- main.py
+++ main.py
@@ -8,23 +8,11 @@ if version_info.major<3 :
 The minimum supported version is Python 3.2')
   exit(1)
 
-from argparse import ArgumentParser
+from args import args
 
-argp = ArgumentParser(description='Plays GIF animation or draws images on to the frame buffer.')
-argp.add_argument('paths', nargs='*', type=str, help='Paths to the directory to scan or to the file to read. Empty list makes it fall into color test mode.')
-argp.add_argument('-ct', '--color-test', action='store_true', help='Beautiful color testing mode')
-argp.add_argument('-nc', '--no-clear', action='store_true', help='Do not clear screen with black on startup and exit.')
-argp.add_argument('-nl', '--no-loop', action='store_true', help='Make entire playlist not to loop forever.')
-argp.add_argument('-nr', '--no-recurse', action='store_true', help='Set to don\'t recurse scan into directories.')
-argp.add_argument('-gd', '--global-delay', type=float, help='This time (seconds) applies to entire static/animated images to show for. Animated files will be infinitely looped while this delay. This value will be applied precedece than the \'-sd\' and \'-ad\', \'-al\'.')
-argp.add_argument('-sd', '--static-delay', type=float, default=20, help='This time (seconds) applies to only static images to show for.')
-argp.add_argument('-al', '--animate-loop', type=int, help='This time (times) applies to only animated images to show for. Animated files will be looped for this count. If it\'s not applied, GIF files will be played for only once of its loop. It precedes \'animate-delay\'.')
-argp.add_argument('-ad', '--animate-delay', type=float, help='This time (seconds) applies to only animated images to show for. Animated files will be infinitely looped while this delay. If it\'s not applied, GIF files will be played for only once of its loop.')
-argp.add_argument('-pv', '--preview', action='store_true', help='Freeze on first frame while loading next frames to preview before playing')
-argp.add_argument('-fb', type=int, default=0, help='Selects frame buffer driver. /dev/fb[-fb] will be used.')
-argp.add_argument('-sf', '--shuffle', action='store_true', help='Shuffle the playlist.')
-args = argp.parse_args()
-
+if args.clear:
+  argp.print_help()
+  exit(0)
 if not args.paths and not args.color_test:
   argp.print_help()
   exit(0)
