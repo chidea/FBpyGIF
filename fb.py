70c475a10cee79e478ae4dd17e38783c122c9e38
ChIdea <sbw228@gmail.com>
2016-08-17 08:12:08 +0900

    memory limit management

--- fb.py
+++ fb.py
@@ -285,11 +285,23 @@ def ready_gif(gif, preview=False):
   from PIL import ImageSequence
   #from multiprocessing import Pool
   imgs = []
+  fm = ''
+  for l in open('/proc/meminfo'):
+    if l.startswith('MemFree:'):
+      fm = l
+      break
+  fm = int(fm.split()[1])*1024
+  frame_limit = fm // (gif.size[0]*gif.size[1]*bpp)
   for img in ImageSequence.Iterator(gif):
     imgs.append(_ready_gif(img))#.copy())
     if preview:
       preview = False
       show_img(imgs[0][0])
+    if len(imgs) >= frame_limit:
+      if _verbose:
+        #print('This file is too big to play. Limited to play only %d frames in total %d frames.' % (frame_limit, gif.n_frames))
+        print('This file is too big to play. Limited to play only %d frames.' % frame_limit)
+      break
   #with Pool(4) as p:
     #imgs=list(p.map(_ready_gif, imgs))
   return imgs
